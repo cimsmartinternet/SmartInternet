a2970dc77fe3f6bae6d44d7b1dd1dfe6602903e9
Arun Jose <arunjose1@gmail.com>
2016-12-20 11:09:58 +0530

    Devices page elements modified

--- Smart_Internet_Auto_QA/src/com/smart_internet/Pages/Devices.java
+++ Smart_Internet_Auto_QA/src/com/smart_internet/Pages/Devices.java
@@ -13,23 +13,25 @@ import org.openqa.selenium.support.How;
  */
 public class Devices 
 {
+	@FindBy(how=How.XPATH,using="//div[contains(@class, 'tabs__nav-title style-scope') and .//text()='Devices']") 
+	WebElement Devices_Tab;
 
-
+		
+	@FindBy(how=How.XPATH,using="//div[@class='si-page-devices__device-list-assigned style-scope si-page-devices']//iron-pages/ul/li/a/div/div") 
+	WebElement Device_Name;
+	
 	@FindBy(how=How.XPATH,using="//button[@class='header-call-to-action style-scope si-page-device-entity']") 
 	WebElement Device_Entity_Page_Change_link;
 	
-	@FindBy(how=How.XPATH,using="(.//*[@id='states']/ul)[2]/li[1]/a/div") 
-	WebElement Device_Name;
-	
-	@FindBy(how=How.XPATH,using="//form[@id='form']/div[1]/div/si-input-text") 
+	@FindBy(how=How.CSS,using="#si-input-text-11") 
 	WebElement Device_Nickname_Box;
 	
-	@FindBy(how=How.XPATH,using=".//*[@id='form']/div[2]/button[1]") 
+	@FindBy(how=How.XPATH,using="//button[contains(@class, 'style-scope si-settings') and .//text()='Apply Changes']") 
 	WebElement Apply_Changes_Button;
 	
-	@FindBy(how=How.XPATH,using=".//*[@id='pages']/div[3]/div/button") 
+	@FindBy(how=How.XPATH,using="(.//*[@id='pages']/div[3]/div/button)[3]") 
 	WebElement Close_Button;
 	
-	@FindBy(how=How.XPATH,using=".//*[@id='pages']/div[3]/div/button") 
+	@FindBy(how=How.XPATH,using="(//*[@class='style-scope si-back-button'])[1]") 
 	WebElement Back_Button;
 }
